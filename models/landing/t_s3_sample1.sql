b28536941f193d4efe5be730c8a6589b0017df1b
LALIT DESHAWAL <deshawal.lalit40@gmail.com>
2024-01-27 12:16:42 +0000

    commit

---
+++ models/landing/t_s3_sample1.sql
@@ -0,0 +1,13 @@
+{{
+    config
+    (
+         materialized='table', transient=false
+    )
+}}
+select $1:continent::varchar as continent,
+              $1:country:name::varchar as name,
+              $1:country:city::variant as city
+      from
+       @SNOWFLAKE_DEMO.LANDING.SNOW_STAGES/SAMPLE
+    
+    
